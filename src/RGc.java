58369436915c67008d07375775b7df4ec83a4309
Andrees03 <andreysergeev.andrei@yandex.ru>
2023-01-16 21:06:51 +0300

    Initial commit

---
+++ src/RGc.java
@@ -0,0 +1,17 @@
+public class RGc {
+    public static void main(String[] args) {
+        int n = 57;
+        int a;
+        a = 5;
+        int b;
+        b = 7;
+        int c;
+        c = a + b;
+        int d;
+        d = a * b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
